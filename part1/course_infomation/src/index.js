9215450500a6af96fbceef3df816ffea8f2a17d4
Ann <ann.nguyen.codes@gmail.com>
2020-07-16 23:32:54 -0400

    part 1.3

--- part1/course_infomation/src/index.js
+++ part1/course_infomation/src/index.js
@@ -7,15 +7,19 @@ import Total from "./components/Total";
 // part 1.1 (note part 1.2 is in the components/Part.js file)
 const App = () => {
   const course = "Half Stack application development";
-
-  // this would be better if it was in some kind of data structure
-  const part1 = "Fundamentals of React";
-  const exercises1 = 10;
-  const part2 = "Using props to pass data";
-  const exercises2 = 7;
-  const part3 = "State of a component";
-  const exercises3 = 14;
-  const exercisesTotals = exercises1 + exercises2 + exercises3;
+  const part1 = {
+    name: 'Fundamentals of React',
+    exercises: 10
+  }
+  const part2 = {
+    name: 'Using props to pass data',
+    exercises: 7
+  }
+  const part3 = {
+    name: 'State of a component',
+    exercises: 14
+  }
+  const exercisesTotals = part1.exercises + part2.exercises + part3.exercises;
 
   return (
     <div>
@@ -24,9 +28,6 @@ const App = () => {
         part1={part1}
         part2={part2}
         part3={part3}
-        exercises1={exercises1}
-        exercises2={exercises2}
-        exercises3={exercises3}
       />
       <Total exercisesTotals={exercisesTotals} />
     </div>
