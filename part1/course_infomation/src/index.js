47eb0e58c94d577845cd2734fec7183ace415f7b
Ann <ann.nguyen.codes@gmail.com>
2020-07-16 23:37:45 -0400

    complete part 1.4

--- part1/course_infomation/src/index.js
+++ part1/course_infomation/src/index.js
@@ -7,27 +7,29 @@ import Total from "./components/Total";
 // part 1.1 (note part 1.2 is in the components/Part.js file)
 const App = () => {
   const course = "Half Stack application development";
-  const part1 = {
-    name: 'Fundamentals of React',
-    exercises: 10
-  }
-  const part2 = {
-    name: 'Using props to pass data',
-    exercises: 7
-  }
-  const part3 = {
-    name: 'State of a component',
-    exercises: 14
-  }
-  const exercisesTotals = part1.exercises + part2.exercises + part3.exercises;
+  const parts = [
+    {
+      name: 'Fundamentals of React',
+      exercises: 10
+    },
+    {
+      name: 'Using props to pass data',
+      exercises: 7
+    },
+    {
+      name: 'State of a component',
+      exercises: 14
+    }
+  ]
+  const exercisesTotals = parts[0].exercises + parts[1].exercises + parts[2].exercises;
 
   return (
     <div>
       <Header course={course} />
       <Content
-        part1={part1}
-        part2={part2}
-        part3={part3}
+        part1={parts[0]}
+        part2={parts[1]}
+        part3={parts[2]}
       />
       <Total exercisesTotals={exercisesTotals} />
     </div>
