f4a40bc2f27dacedbe0db34c13923cdd5e221cde
Ann <ann.nguyen.codes@gmail.com>
2020-07-19 00:12:43 -0400

    add part 1.7

--- part1/course_infomation/src/index.js
+++ part1/course_infomation/src/index.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, {useState} from "react";
 import ReactDOM from "react-dom";
 import Header from "./components/Header.js";
 import Content from "./components/Content.js";
@@ -8,28 +8,42 @@ import Total from "./components/Total";
 const App = () => {
   // part 1.5
   const course = {
-    name: 'Half Stack application development',
+    name: "Half Stack application development",
     parts: [
       {
-        name: 'Fundamentals of React',
-        exercises: 10
+        name: "Fundamentals of React",
+        exercises: 10,
       },
       {
-        name: 'Using props to pass data',
-        exercises: 7
+        name: "Using props to pass data",
+        exercises: 7,
       },
       {
-        name: 'State of a component',
-        exercises: 14
-      }
-    ]
-  }
+        name: "State of a component",
+        exercises: 14,
+      },
+    ],
+  };
+
+  const [value, setValue] = useState(0);
+
+  // notes: returninig a function within a function
+  const setToValue = (newValue) => () => {
+    setValue(newValue);
+  };
 
   return (
     <div>
       <Header course={course.name} />
       <Content parts={course.parts} />
       <Total totals={course.parts} />
+
+      <div>
+        {value}
+        <button onClick={setToValue(1000)}>thousand</button>
+        <button onClick={setToValue(0)}>reset</button>
+        <button onClick={setToValue(value + 1)}>increment</button>
+      </div>
     </div>
   );
 };
