c5f37076cd096a92d646c4eb252c2bbfbad04b19
Ann <ann.nguyen.codes@gmail.com>
2020-07-19 23:04:26 -0400

    add part 1.14

--- part1/anecdotes/src/components/App.js
+++ part1/anecdotes/src/components/App.js
@@ -7,6 +7,23 @@ const App = (props) => {
 
   const { anecdotes } = props;
 
+  //   let win = -1;
+  //   let hiVotes = -1;
+  //   for (let key in points) {
+  //     if (points[key] > hiVotes) {
+  //       win = key;
+  //       hiVotes = points[key];
+  //     }
+  //   }
+
+  // same as above that is commented out, but using reduce for practice
+  let winner = Object.keys(points).reduce((highest, current) => {
+    if (!points[highest] || points[current] > points[highest]) {
+      highest = current;
+    }
+    return highest;
+  }, -1);
+
   const getRandIndex = () => {
     const randIndex = Math.floor(anecdotes.length * Math.random());
     setSelected(randIndex);
@@ -20,12 +37,25 @@ const App = (props) => {
     }
   };
 
+  const winnerText = points[winner] ? (
+    <p>
+      This anecdote is the winner with {points[winner] ? points[winner] : 0}{" "}
+      votes.
+    </p>
+  ) : (
+    <p>Go vote!</p>
+  );
+
   return (
     <div>
+      <h1>Anecdote of the Day</h1>
       <p>{anecdotes[selected]}</p>
       <p>This anecdote has {points[selected] ? points[selected] : 0} votes.</p>
       <Button handleClick={voteForAnecdote} buttonName="Vote" />
       <Button handleClick={getRandIndex} buttonName="Next Anecdote" />
+      <h1>Winning Anecdote</h1>
+      <p>{anecdotes[winner] ? anecdotes[winner] : "Which quote will win?"}</p>
+      {winnerText}
     </div>
   );
 };
