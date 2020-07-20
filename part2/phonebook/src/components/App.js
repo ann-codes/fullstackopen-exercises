3c0aefa0f619ef085ffaeb2664b7e6113f435a52
Ann <ann.nguyen.codes@gmail.com>
2020-07-20 17:40:45 -0400

    add part 2.7

--- part2/phonebook/src/components/App.js
+++ part2/phonebook/src/components/App.js
@@ -3,16 +3,27 @@ import React, { useState } from "react";
 const App = () => {
   const [persons, setPersons] = useState([{ name: "Arto Hellas" }]);
   const [newName, setNewName] = useState("");
+  const [isDupe, setIsDupe] = useState(false);
 
   const handleNameChange = (e) => {
     console.log(e.target.value);
+    setIsDupe(false);
     setNewName(e.target.value);
   };
 
   const submitName = (e) => {
     e.preventDefault();
-    setPersons([...persons, { name: newName }]);
-    setNewName("")
+
+    const findName = persons.find(
+      (person) => person.name.toLowerCase() === newName.toLowerCase()
+    );
+
+    if (findName) {
+      setIsDupe(true);
+    } else {
+      setPersons([...persons, { name: newName }]);
+      setNewName("");
+    }
   };
 
   return (
@@ -20,7 +31,12 @@ const App = () => {
       <h2>PhoneBook</h2>
       <form onSubmit={submitName}>
         <div>
-          name: <input value={newName} onChange={handleNameChange} />
+          <p>
+            {isDupe ? `${newName} is a Duplicate! Try again!` : "Add a Name:"}
+          </p>
+          <p>
+            name: <input value={newName} onChange={handleNameChange} />
+          </p>
         </div>
         <div>
           <button type="submit">add</button>
