4f2f4c346d580bfafdbe40cbe729b5c11f3cf9a8
Ann <ann.nguyen.codes@gmail.com>
2020-07-20 18:04:55 -0400

    add part 2.8

--- part2/phonebook/src/components/App.js
+++ part2/phonebook/src/components/App.js
@@ -1,30 +1,43 @@
 import React, { useState } from "react";
 
 const App = () => {
-  const [persons, setPersons] = useState([{ name: "Arto Hellas" }]);
-  const [newName, setNewName] = useState("");
+  const defaultPersons = [
+    { name: "Arto Hellas", number: "040-123456" },
+    { name: "Ada Lovelace", number: "39-44-5323523" },
+    { name: "Dan Abramov", number: "12-43-234345" },
+    { name: "Mary Poppendieck", number: "39-23-6423122" },
+  ];
+
+  const [persons, setPersons] = useState(defaultPersons);
+  const [newName, setNewName] = useState({ name: "", number: "" });
   const [isDupe, setIsDupe] = useState(false);
 
   const handleNameChange = (e) => {
-    console.log(e.target.value);
     setIsDupe(false);
-    setNewName(e.target.value);
+    console.log(newName);
+    setNewName({ ...newName, [e.target.name]: e.target.value });
   };
 
   const submitName = (e) => {
     e.preventDefault();
 
     const findName = persons.find(
-      (person) => person.name.toLowerCase() === newName.toLowerCase()
+      (person) => person.name.toLowerCase() === newName.name.toLowerCase()
     );
 
     if (findName) {
       setIsDupe(true);
     } else {
-      setPersons([...persons, { name: newName }]);
-      setNewName("");
+      setPersons([...persons, newName]);
+      setNewName({ name: "", number: "" });
     }
   };
+
+  const mapPhonebook = persons.map((person, i) => (
+    <li key={i}>
+      {person.name}: {person.number}
+    </li>
+  ));
 
   return (
     <div>
@@ -32,10 +45,25 @@ const App = () => {
       <form onSubmit={submitName}>
         <div>
           <p>
-            {isDupe ? `${newName} is a Duplicate! Try again!` : "Add a Name:"}
+            {isDupe
+              ? `${newName.name} is a Duplicate! Try again!`
+              : "Add a Name and Number:"}
           </p>
           <p>
-            name: <input value={newName} onChange={handleNameChange} />
+            name:{" "}
+            <input
+              name="name"
+              value={newName.name}
+              onChange={handleNameChange}
+            />
+          </p>
+          <p>
+            number:{" "}
+            <input
+              name="number"
+              value={newName.number}
+              onChange={handleNameChange}
+            />
           </p>
         </div>
         <div>
@@ -43,7 +71,7 @@ const App = () => {
         </div>
       </form>
       <h2>Numbers</h2>
-      ...
+      <ul>{mapPhonebook}</ul>
     </div>
   );
 };
