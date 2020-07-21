0541198b8f399a45f0262c9928e7e15034edddf1
Ann <ann.nguyen.codes@gmail.com>
2020-07-20 21:21:46 -0400

    setup next assignment

---
+++ part2/countries_data/src/index.js
@@ -0,0 +1,11 @@
+import React from "react";
+import ReactDOM from "react-dom";
+
+import App from "./components/App";
+
+ReactDOM.render(<App />, document.getElementById("root"));
+
+
+// // === example from reading; comment out above two lines to see it working
+// import App from "./components/example/App"
+// ReactDOM.render(<App />, document.getElementById("root"));
