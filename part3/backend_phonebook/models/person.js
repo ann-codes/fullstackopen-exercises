06776ba13c777e69219ba29b730772c012a949c1
Ann <ann.nguyen.codes@gmail.com>
2020-08-01 00:09:16 -0400

    updates

--- part3/backend_phonebook/models/person.js
+++ part3/backend_phonebook/models/person.js
@@ -2,7 +2,7 @@ const mongoose = require("mongoose");
 
 const url = process.env.MONGODB_URI;
 
-mongoose.set('useFindAndModify', false)
+mongoose.set("useFindAndModify", false);
 mongoose
   .connect(url, { useNewUrlParser: true, useUnifiedTopology: true })
   .then((result) => {
@@ -14,39 +14,39 @@ mongoose
 
 // SCHEMA
 const personSchema = new mongoose.Schema({
-  name: { type: String, required: true, unique: true },
-  number: { type: String, required: true}
+  name: { type: String, required: true, unique: true, minlength: 2 },
+  number: { type: String, required: true, minlength: 5 },
 });
 
 const Person = mongoose.model("Person", personSchema);
 
 // convert the mongo _id to id and
 // deleting the mongo _id and __v obj from displaying in api endpoint
-personSchema.set('toJSON', {
+personSchema.set("toJSON", {
   transform: (document, returnedObject) => {
-      returnedObject.id = returnedObject._id.toString()
-      delete returnedObject._id
-      delete returnedObject.__v
-  }
-})
+    returnedObject.id = returnedObject._id.toString();
+    delete returnedObject._id;
+    delete returnedObject.__v;
+  },
+});
 
 // Data to seed
 let persons = [
   {
     name: "Arto Hellas",
-    number: "040-123456"
+    number: "040-123456",
   },
   {
     name: "Ada Lovelace",
-    number: "39-44-5323523"
+    number: "39-44-5323523",
   },
   {
     name: "Dan Abramov",
-    number: "12-43-234345"
+    number: "12-43-234345",
   },
   {
     name: "Mary Poppendieck",
-    number: "39-23-6423122"
+    number: "39-23-6423122",
   },
 ];
 
@@ -59,12 +59,15 @@ const seedPersonDB = (data) => {
       id: person.id,
     });
 
-    addPerson.save().then((result) => {
-      console.log(`Seeding ${person.name} (${person.number}) to phonebook!`);
-      mongoose.connection.close();
-    }).catch(err => {
-      console.log("Error in data seed:", err.message);
-    });
+    addPerson
+      .save()
+      .then((result) => {
+        console.log(`Seeding ${person.name} (${person.number}) to phonebook!`);
+        mongoose.connection.close();
+      })
+      .catch((err) => {
+        console.log("Error in data seed:", err.message);
+      });
   });
 };
 
