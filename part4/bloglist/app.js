76d0ab6c81df34e72abb8d6ff95af94b4b5cb42a
Ann <ann.nguyen.codes@gmail.com>
2020-08-15 01:25:04 -0400

    parts 4.20-4.21

--- part4/bloglist/app.js
+++ part4/bloglist/app.js
@@ -3,6 +3,7 @@ const express = require("express");
 require("express-async-errors");
 const app = express();
 const cors = require("cors");
+const middleware = require("./utils/middleware");
 const blogRouter = require("./controllers/blogs");
 const usersRouter = require("./controllers/users");
 const loginRouter = require("./controllers/login");
@@ -28,7 +29,11 @@ app.use(cors());
 app.use(express.static("build"));
 app.use(express.json());
 
-// defining the primary path
+// middleware
+app.use(middleware.requestLogger);
+app.use(middleware.tokenExtractor);
+
+// defining the paths
 app.use("/api/blogs", blogRouter);
 app.use("/api/users", usersRouter);
 app.use("/api/login", loginRouter);
