3e65b41b8ede10a08b9de725bdd171c348659607
Ann <ann.nguyen.codes@gmail.com>
2020-09-16 00:26:05 -0400

    complete parts 8.8-8.12

--- part8/library-frontend/src/components/Authors.js
+++ part8/library-frontend/src/components/Authors.js
@@ -1,38 +1,89 @@
-  
-import React from 'react'
+import React, { useState, useEffect } from "react";
+import Select from "react-select";
+import { useQuery, useMutation } from "@apollo/client";
+import { ALL_AUTHORS, EDIT_AUTHOR } from "../queries";
 
 const Authors = (props) => {
+  const [authors, setAuthors] = useState([]);
+  const [authorName, setAuthorName] = useState("");
+  const [authorBorn, setAuthorBorn] = useState("");
+  const [updateAuthor, resultUpdate] = useMutation(EDIT_AUTHOR);
+  const resultAll = useQuery(ALL_AUTHORS);
+
+  useEffect(() => {
+    if (resultAll.data) {
+      setAuthors(resultAll.data.allAuthors);
+    }
+  }, [resultAll.data]);
+
+  useEffect(() => {
+    if (resultUpdate.data && !resultUpdate.data.editAuthor) {
+      console.error("NAME NOT FOUND");
+    }
+  }, [resultUpdate.data]);
+
+  const updateAuthorSubmit = async (e) => {
+    e.preventDefault();
+    updateAuthor({
+      variables: { name: authorName.value, setBornTo: parseInt(authorBorn) },
+    });
+    setAuthorName("");
+    setAuthorBorn("");
+  };
+
   if (!props.show) {
-    return null
+    return null;
   }
-  const authors = []
+
+  if (resultAll.loading) {
+    return <div>loading...</div>;
+  }
+
+  const options = authors.map((a) => {
+    return { value: a.name, label: a.name };
+  });
 
   return (
     <div>
-      <h2>authors</h2>
+      <h2>Authors</h2>
       <table>
         <tbody>
           <tr>
             <th></th>
-            <th>
-              born
-            </th>
-            <th>
-              books
-            </th>
+            <th>Year Born</th>
+            <th>Book Count</th>
           </tr>
-          {authors.map(a =>
+          {authors.map((a) => (
             <tr key={a.name}>
               <td>{a.name}</td>
               <td>{a.born}</td>
               <td>{a.bookCount}</td>
             </tr>
-          )}
+          ))}
         </tbody>
       </table>
-
+      <h2>Set Birth Year</h2>
+      <form onSubmit={updateAuthorSubmit}>
+        <div style={{ width: "250px" }}>
+          Name:{" "}
+          <Select
+            defaultValue={authorName}
+            onChange={setAuthorName}
+            options={options}
+          />
+        </div>
+        <div>
+          Born:{" "}
+          <input
+            type="number"
+            value={authorBorn}
+            onChange={({ target }) => setAuthorBorn(target.value)}
+          />
+        </div>
+        <button type="submit">Update Author</button>
+      </form>
     </div>
-  )
-}
+  );
+};
 
-export default Authors
+export default Authors;
---
+++ part8/library-frontend/src/queries.js
@@ -0,0 +1,57 @@
+import { gql } from "@apollo/client";
+
+export const ALL_AUTHORS = gql`
+  query {
+    allAuthors {
+      name
+      born
+      id
+      bookCount
+    }
+  }
+`;
+
+export const ALL_BOOKS = gql`
+  query {
+    allBooks {
+      title
+      author
+      published
+      genres
+      id
+    }
+  }
+`;
+
+export const ADD_BOOK = gql`
+  mutation addBook(
+    $title: String!
+    $published: Int!
+    $author: String!
+    $genres: [String!]!
+  ) {
+    addBook(
+      title: $title
+      published: $published
+      author: $author
+      genres: $genres
+    ) {
+      title
+      published
+      author
+      genres
+      id
+    }
+  }
+`;
+
+export const EDIT_AUTHOR = gql`
+  mutation editAuthor($name: String!, $setBornTo: Int!) {
+    editAuthor(name: $name, setBornTo: $setBornTo) {
+      name
+      born
+      id
+      bookCount
+    }
+  }
+`;
