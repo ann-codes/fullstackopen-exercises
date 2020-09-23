0bc00110471eb8406a297a7ea0ba511485ad6527
Ann <ann.nguyen.codes@gmail.com>
2020-09-23 01:13:40 -0400

    complete part 8.16

--- part8/library-backend/config.js
+++ part8/library-backend/config.js
@@ -2,6 +2,6 @@ require("dotenv").config();
 
 let PORT = process.env.PORT;
 let MONGODB_URI = process.env.MONGODB_URI;
-let JWT_SECRET = process.env.JWT_SECRET
+let JWT_SECRET = process.env.JWT_SECRET;
 
-module.exports = { MONGODB_URI, PORT };
+module.exports = { MONGODB_URI, PORT, JWT_SECRET };
