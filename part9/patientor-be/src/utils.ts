ff2decf83fc3da37b55dc0e955a3d8c68f9cc36b
Ann <ann.nguyen.codes@gmail.com>
2020-10-07 01:12:55 -0400

    complete part 9.13

--- part9/patientor-be/src/utils.ts
+++ part9/patientor-be/src/utils.ts
@@ -1,21 +1,40 @@
 /* eslint-disable @typescript-eslint/no-explicit-any */
 /* eslint-disable @typescript-eslint/restrict-plus-operands */
 /* eslint-disable @typescript-eslint/no-unsafe-member-access */
+/* eslint-disable @typescript-eslint/explicit-module-boundary-types */
+/* eslint-disable @typescript-eslint/restrict-template-expressions*/
 
-import { NewPatient } from "./types";
+import { NewPatient, Gender } from "./types";
 
-// const isString = (text: any): text is string => {
-//   return typeof text === "string" || text instanceof String;
-// };
+const isString = (text: any): text is string => {
+  return typeof text === "string" || text instanceof String;
+};
+
+const parseStringType = (element: any, typeName: string): string => {
+  if (!element || !isString(element)) {
+    throw new Error(`Incorrect or missing ${typeName}: ${element}`);
+  }
+  return element;
+};
+
+const isGender = (param: any): param is Gender => {
+  return Object.values(Gender).includes(param);
+};
+
+const parseGender = (gender: any): Gender => {
+  if (!gender || !isGender(gender)) {
+    throw new Error("Incorrect or missing gender: " + gender);
+  }
+  return gender;
+};
 
 const toNewPatient = (object: any): NewPatient => {
-  console.log(object); ////
   return {
-    name: "test!",
-    dateOfBirth: "",
-    ssn: "",
-    gender: "",
-    occupation: "",
+    name: parseStringType(object.name, "name"),
+    dateOfBirth: parseStringType(object.dateOfBirth, "DOB"),
+    ssn: parseStringType(object.ssn, "ssn"),
+    gender: parseGender(object.gender),
+    occupation: parseStringType(object.occupation, "occupation"),
   };
 };
 
